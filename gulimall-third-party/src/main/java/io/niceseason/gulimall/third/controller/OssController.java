cca0455a0ecc0137d7b9097bbeee45647d57aeb7
yfqian <yfqian@dces.cn>
2023-04-16 10:38:40 +0800

    增加minio服务

--- gulimall-third-party/src/main/java/io/niceseason/gulimall/third/controller/OssController.java
+++ gulimall-third-party/src/main/java/io/niceseason/gulimall/third/controller/OssController.java
@@ -1,19 +1,26 @@
 package io.niceseason.gulimall.third.controller;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.IdUtil;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
 import io.niceseason.common.utils.R;
+import io.niceseason.gulimall.third.config.MinioTemplate;
+import io.niceseason.gulimall.third.config.OssConfig;
+import io.niceseason.gulimall.third.constant.OssType;
+import io.niceseason.gulimall.third.vo.OssVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class OssController {
@@ -30,6 +37,49 @@ public class OssController {
     String accessId ;
     @Value("${spring.cloud.alicloud.secret-key}")
     String accessKey ;
+    private static final Logger log = LoggerFactory.getLogger(OssController.class);
+
+    /**
+     * 上传的文件夹(根据时间确定)
+     */
+    public static final String NORM_DAY_PATTERN = "yyyy/MM/dd";
+
+    @Autowired
+    private OssConfig ossConfig;
+    @Autowired
+    private MinioTemplate minioTemplate;
+
+
+    @GetMapping(value = "/oss/info")
+    public R info(@RequestParam("fileNum") Integer fileNum) {
+        OssVO ossVO = new OssVO();
+        // minio文件上传
+        if (Objects.equals(ossConfig.getOssType(), OssType.MINIO.value())) {
+            fillMinIoInfo(ossVO, fileNum);
+        }
+        return R.ok().setData(ossVO);
+    }
+
+    private void fillMinIoInfo(OssVO ossVo, Integer fileNum) {
+        List<OssVO> ossVOList = new ArrayList<>();
+        for (int i = 0; i<fileNum; i++) {
+            OssVO oss = loadOssVO(new OssVO());
+            String actionUrl = minioTemplate.getPresignedObjectUrl(oss.getDir() + oss.getFileName());
+            oss.setActionUrl(actionUrl);
+            ossVOList.add(oss);
+        }
+        ossVo.setOssList(ossVOList);
+    }
+
+    private OssVO loadOssVO(OssVO ossVo) {
+        String dir = DateUtil.format(new Date(), NORM_DAY_PATTERN)+ "/";
+        String fileName = IdUtil.simpleUUID();
+        ossVo.setDir(dir);
+        ossVo.setFileName(fileName);
+        return ossVo;
+    }
+
+
     @RequestMapping("/oss/policy")
     public R policy(){
 
